d154a0ff9f81a996e174d8b9957a773586ca0782
Spencer Young <spencerwyoung@outlook.com>
2018-10-29 21:47:00 -0600

    add sender parameter, take function, and documentation comments

--- src/SingleEvent.ts
+++ src/SingleEvent.ts
@@ -1,21 +1,23 @@
-type Listener<A> = (args: A) => void;
+type Listener<A, S> = (args: A, src: S) => void;
 
-abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
-    private readonly _listeners = new Set<Listener<B>>();
-    private readonly _children = new Set<SuperEvent<B, any>>();
+abstract class SuperEvent<A, B, S> implements singleEvent.SingleEvent<B, S> {
+    private readonly _listeners = new Set<Listener<B, S>>();
+    private readonly _children = new Set<SuperEvent<B, any, S>>();
     private readonly _connect: () => void;
     private readonly _disconnect: () => void;
+    private readonly _sender: S;
 
     protected readonly _boundTrigger = (args: A) => this._trigger(args);
 
-    protected constructor(connect: () => void, disconnect: () => void) {
+    protected constructor(sender: S, connect: () => void, disconnect: () => void) {
+        this._sender = sender;
         this._connect = connect;
         this._disconnect = disconnect;
     }
 
     protected _notifyListeners(args: B) {
         for (let l of this._listeners) {
-            l(args);
+            l(args, this._sender);
         }
         for (let c of this._children) {
             c._trigger(args);
@@ -24,14 +26,14 @@ abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
 
     protected abstract _trigger(args: A): void;
 
-    public listen(listener: Listener<B>) {
+    public listen(listener: Listener<B, S>) {
         if (this._listeners.size === 0 && this._children.size === 0) {
             this._connect();
         }
         this._listeners.add(listener);
     }
 
-    public unlisten(listener: Listener<any>) {
+    public unlisten(listener: Listener<any, S>) {
         this._listeners.delete(listener);
         for (let child of this._children) {
             child.unlisten(listener);
@@ -41,14 +43,14 @@ abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
         }
     }
 
-    protected addChild(e: SuperEvent<B, any>) {
+    protected addChild(e: SuperEvent<B, any, S>) {
         if (this._listeners.size === 0 && this._children.size === 0) {
             this._connect();
         }
         this._children.add(e);
     }
 
-    protected removeChild(e: SuperEvent<B, any>) {
+    protected removeChild(e: SuperEvent<B, any, S>) {
         this._children.delete(e);
         if (this._listeners.size === 0 && this._children.size === 0) {
             this._disconnect();
@@ -56,7 +58,7 @@ abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
     }
 
     public next(): Promise<B> {
-        let l: Listener<B>;
+        let l: Listener<B, S>;
         return new Promise(ok => {
             l = a => {
                 ok(a);
@@ -67,8 +69,9 @@ abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
     }
 
     public filter(pred: (args: B) => boolean) {
-        let filterEvent: FilterEvent<B>;
+        let filterEvent: FilterEvent<B, S>;
         filterEvent = new FilterEvent(
+            this._sender,
             () => this.addChild(filterEvent),
             () => this.removeChild(filterEvent),
             pred,
@@ -77,32 +80,42 @@ abstract class SuperEvent<A, B> implements singleEvent.SingleEvent<B> {
     }
 
     public map<C>(transform: (args: B) => C) {
-        let mapEvent: MapEvent<B, C>;
+        let mapEvent: MapEvent<B, C, S>;
         mapEvent = new MapEvent(
+            this._sender,
             () => this.addChild(mapEvent),
             () => this.removeChild(mapEvent),
             transform,
         );
         return mapEvent;
     }
+
+    public async take(count: number): Promise<B[]> {
+        let array: B[] = [];
+        for (let i = 0; i < count; ++i) {
+            array.push(await this.next());
+        }
+        return array;
+    }
 }
 
-class SingleEventSource<A> extends SuperEvent<A, A> {
+class SingleEventSource<A, S> extends SuperEvent<A, A, S> {
     protected _trigger(args: A) {
         this._notifyListeners(args);
     }
 
-    public static create<A>(connect: () => void, disconnect: () => void): [SingleEventSource<A>, Listener<A>] {
-        let event = new SingleEventSource<A>(connect, disconnect);
-        return [event, a => { event._trigger(a) }];
+    public static create<A, S = undefined>(sender: S, connect: () => void, disconnect: () => void):
+        [SingleEventSource<A, S>, (a: A) => void] {
+        let event = new SingleEventSource<A, S>(sender, connect, disconnect);
+        return [event, a => event._trigger(a)];
     }
 }
 
-class FilterEvent<A> extends SuperEvent<A, A> {
+class FilterEvent<A, S> extends SuperEvent<A, A, S> {
     private readonly _pred: (args: A) => boolean;
 
-    public constructor(connect: () => void, disconnect: () => void, pred: (args: A) => boolean) {
-        super(connect, disconnect);
+    public constructor(sender: S, connect: () => void, disconnect: () => void, pred: (args: A) => boolean) {
+        super(sender, connect, disconnect);
         this._pred = pred;
     }
 
@@ -113,11 +126,11 @@ class FilterEvent<A> extends SuperEvent<A, A> {
     }
 }
 
-class MapEvent<A, B> extends SuperEvent<A, B> {
+class MapEvent<A, B, S> extends SuperEvent<A, B, S> {
     private readonly _transform: (args: A) => B;
 
-    public constructor(connect: () => void, disconnect: () => void, transform: (args: A) => B) {
-        super(connect, disconnect);
+    public constructor(sender: S, connect: () => void, disconnect: () => void, transform: (args: A) => B) {
+        super(sender, connect, disconnect);
         this._transform = transform;
     }
 
@@ -126,16 +139,103 @@ class MapEvent<A, B> extends SuperEvent<A, B> {
     }
 }
 
-export function singleEvent<A>(connect = () => { }, disconnect = () => { }): [singleEvent.SingleEvent<A>, Listener<A>] {
-    return SingleEventSource.create(connect, disconnect);
+/**
+ * Creates an instance of SingleEvent and a function that triggers the event.
+ *
+ * @export
+ * @template A
+ * The type of argument that will be used to trigger the event and propagate to all event listeners.
+ * @template S
+ * A type representing the sender of the event.
+ * @param {S} sender
+ * This value will be passed to listeners each time the event fires.
+ * This can be used to distinguish the source of the event when the same listener is added to
+ * multiple events.
+ * @param connect
+ * This function will be called any time a listener is added when the event previously had no
+ * listeners.
+ * This can be used to perform setup operations or aquire resources that are only needed while the
+ * event has listeners.
+ * @param disconnect
+ * This function will be called when a listener is removed and as a result the event no longer has
+ * any listeners.
+ * This can be used to perform cleanup operations and release resources that are only needed while
+ * the event has listeners.
+ * @returns {[singleEvent.SingleEvent<A, S>, (a: A) => void]}
+ */
+export function singleEvent<A, S = undefined>(sender: S, connect = () => { }, disconnect = () => { }): [singleEvent.SingleEvent<A, S>, (a: A) => void] {
+    return SingleEventSource.create(sender, connect, disconnect);
 }
 
 export namespace singleEvent {
-    export interface SingleEvent<A> {
-        listen(l: Listener<A>): void;
-        unlisten(l: Listener<A>): void;
+    /**
+     * An object that emits a single type of event.
+     * When the event is fired, all listeners added to the event will be invoked.
+     *
+     * @export
+     * @interface SingleEvent
+     * @template A
+     * @template S
+     */
+    export interface SingleEvent<A, S = undefined> {
+        /**
+         * Adds an event listener.
+         *
+         * @param l
+         * The listener to add.
+         * 
+         * @memberof SingleEvent
+         */
+        listen(l: Listener<A, S>): void;
+        /**
+         * Removes an event listener.
+         *
+         * @param l
+         * The listener to remove.
+         * If it was previously added as a listener of the event, it will no longer be invoked when
+         * the event fires.
+         * Additionally the listener will be removed from any child events.
+         * @memberof SingleEvent
+         */
+        unlisten(l: Listener<A, S>): void;
+        /**
+         * Returns a promise that will be fulfilled the next time the event fires.
+         * 
+         * @memberof SingleEvent
+         */
         next(): Promise<A>;
-        filter(pred: (args: A) => boolean): SingleEvent<A>;
-        map<B>(transform: (args: A) => B): SingleEvent<B>;
+        /**
+         * Returns a child of this event where listeners are only invoked when the event arguments
+         * are accepted by a predicate function.
+         *
+         * @param pred
+         * The predicate function.
+         * It should return `true` if the listeners should be invoked, and otherwise return `false`.
+         * @memberof SingleEvent
+         */
+        filter(pred: (args: A) => boolean): SingleEvent<A, S>;
+        /**
+         * Returns a child of this event where event arguments are passed through a transformation
+         * function before being passed to the listener.
+         *
+         * @template B
+         * @param transform
+         * The transformation function.
+         * It should the event argument of the parent event and return the argument that will be
+         * passed to the listeners of the child event.
+         * @memberof SingleEvent
+         */
+        map<B>(transform: (args: A) => B): SingleEvent<B, S>;
+        /**
+         * After the event has been fired a specified number of times, return an array of arguments
+         * for each time the event fired.
+         *
+         * @param count
+         * The number of events to wait for until the promise is fulfilled.
+         * @returns
+         * A promise yielding a list of event arguments--one for each time the event was fired.
+         * @memberof SingleEvent
+         */
+        take(count: number): Promise<A[]>;
     }
 }
